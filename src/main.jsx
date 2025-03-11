0336c08c52272f8c11331755515d35e258d7f28f
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-03-11 16:15:31 +0700

    edit: main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from './App.jsx'
+import HomeClass from "./pages/Class.jsx";
+import HomeFunc from "./pages/Function.jsx";
 
-createRoot(document.getElementById('root')).render(
+const person = {
+  name: "Iman",
+  gender: "Pria",
+  umur: 30,
+  isMarried: true,
+  hobbies: ["Tidur", "Koding", "Nonton"],
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    {/* <HomeClass /> */}
+    <HomeFunc
+      name={person.name}
+      gender={person.gender}
+      umur={person.umur}
+      isMarried={person.isMarried}
+      hobbies={person.hobbies}
+      komponen={HomeClass}
+    />
+  </StrictMode>
+);
