dd974c9a05c489b0dc32c156ab0f0b07e34c952c
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-03-12 13:50:02 +0700

    install: tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
     nodePolyfills({
       include: ["events"],
     }),
+    tailwindcss(),
   ],
 });
