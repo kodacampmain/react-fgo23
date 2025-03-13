75eb160b207037245e6efe797aa793805e430486
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-03-13 13:30:32 +0700

    edit: migrate todolist style from css to tailwind

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,16 +2,16 @@ import { Link } from "react-router";
 
 function TodoList({ todos }) {
   return (
-    <section className="todolist">
+    <section className="bg-[blanchedalmond] flex-1 grid grid-cols-(--grid-template-column) auto-rows-(--grid-auto-rows) py-2.5 px-25 gap-1.25">
       {todos.length > 0 &&
         todos.map((todo, idx) => {
           return (
             <Link key={idx} to={`../todo/${idx}`}>
-              <div className="card">
-                <div className="title">
-                  <p>{todo.title}</p>
+              <div className="border-2 border-black p-2.5 flex flex-col transition-all duration-300 cursor-pointer h-full hover:shadow-md hover:shadow-black">
+                <div className="text-[1.3rem] border-b-1 border-b-black pb-2.5 h-8.5">
+                  <p className="overflow-hidden overflow-ellipsis whitespace-nowrap">{todo.title}</p>
                 </div>
-                <div className="body">
+                <div className="pt-2.5 flex-1 text-[0.8rem] overflow-hidden">
                   <p>{todo.body}</p>
                 </div>
               </div>
