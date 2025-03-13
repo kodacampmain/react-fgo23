4ea390973090cae2bf30334ea623e592102c9862
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-03-13 14:10:19 +0700

    moving: router from main to router

--- src/main.jsx
+++ src/main.jsx
@@ -1,64 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router";
+import { BrowserRouter } from "react-router";
+
+import Router from "./router.jsx";
 import "./styles/tailwind.css";
 
 // import "./index.css";
 // import App from './App.jsx'
-import HomeClass from "./pages/Class.jsx";
-import HomeFunc from "./pages/Function.jsx";
-import Header from "./components/Header.jsx";
-import Todo from "./pages/Todo";
-import TodoDetail from "./pages/TodoDetail.jsx";
-
-const person = {
-  name: "Iman",
-  gender: "Pria",
-  umur: 30,
-  isMarried: true,
-  hobbies: ["Tidur", "Koding", "Nonton"],
-};
-
-function Layout() {
-  return (
-    <>
-      <Header />
-      <Outlet />
-    </>
-  );
-}
-
-function RouteError() {
-  return <h1>Route Error</h1>;
-}
-
-function Router() {
-  const { name, gender, umur, isMarried, hobbies } = person;
-  return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route
-          index
-          element={
-            <HomeFunc
-              name={name}
-              gender={gender}
-              umur={umur}
-              isMarried={isMarried}
-              hobbies={hobbies}
-              komponen={HomeClass}
-            />
-          }
-        />
-        <Route path="todo">
-          <Route index element={<Todo />} />
-          <Route path=":id" element={<TodoDetail />} />
-        </Route>
-      </Route>
-      <Route path="*" element={<RouteError />} />
-    </Routes>
-  );
-}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,57 @@
+import { Routes, Route, Outlet } from "react-router";
+import HomeClass from "./pages/Class.jsx";
+import HomeFunc from "./pages/Function.jsx";
+import Header from "./components/Header.jsx";
+import Todo from "./pages/Todo";
+import TodoDetail from "./pages/TodoDetail.jsx";
+
+const person = {
+  name: "Iman",
+  gender: "Pria",
+  umur: 30,
+  isMarried: true,
+  hobbies: ["Tidur", "Koding", "Nonton"],
+};
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
+
+function RouteError() {
+  return <h1>Route Error</h1>;
+}
+
 function Router() {
-  return <div>router</div>;
+  const { name, gender, umur, isMarried, hobbies } = person;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route
+          index
+          element={
+            <HomeFunc
+              name={name}
+              gender={gender}
+              umur={umur}
+              isMarried={isMarried}
+              hobbies={hobbies}
+              komponen={HomeClass}
+            />
+          }
+        />
+        <Route path="todo">
+          <Route index element={<Todo />} />
+          <Route path=":id" element={<TodoDetail />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<RouteError />} />
+    </Routes>
+  );
 }
 
 export default Router;
