6b846cf3cd3c1b6e0e59308228fb94e26b4106bb
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-03-19 17:06:32 +0700

    edit: add config for ignoring serializableCheck

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,56 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistCombineReducers,
+  persistReducer,
+  REGISTER,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import todoReducer from "./slices/todo";
 import usersReducer from "./slices/users";
 import constant from "../configs/constant";
 
-const todoPersistConfig = {
-  key: "fgo23:todo-store",
+// const todoPersistConfig = {
+//   key: "fgo23:todo-store",
+//   storage,
+// };
+
+// const persistedTodoReducer = persistReducer(todoPersistConfig, todoReducer);
+
+const persistConfig = {
+  key: "fgo23:store",
   storage,
+  blacklist: ["users"],
 };
 
-const persistedTodoReducer = persistReducer(todoPersistConfig, todoReducer);
+const usersPersistConfig = {
+  key: "fgo23:store:users",
+  storage,
+  whitelist: ["data"],
+};
+
+const persistedReducers = persistCombineReducers(persistConfig, {
+  todo: todoReducer,
+  users: persistReducer(usersPersistConfig, usersReducer),
+});
 
 const store = configureStore({
   // pada createStore, reducer jika lebih dari 1 digabungkan menggunakan combineReducer
-  reducer: {
-    todo: persistedTodoReducer,
-    users: usersReducer,
-  },
+  reducer: persistedReducers,
   devTools: constant.nodeEnv !== "production",
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [REGISTER, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE],
+      },
+    });
+  },
 });
 
 export const persistedStore = persistStore(store);
